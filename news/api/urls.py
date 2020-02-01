8f55f0645d196bcb3c5e04189d50f9906085d341
infomotin <touch.u.bc1000@gmail.com>
2020-02-01 17:30:38 +0600

    class Based Views

--- news/api/urls.py
+++ news/api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
-from news.api.views import listCreateArticleSerializer, article_detais_views
+from news.api.views import (listCreateArticleSerializer,
+                            article_detais_views,
+                            ArticleListViewsCreateViews,
+                            ArticleDetailView)
 
 urlpatterns = [
     path("api-list/", listCreateArticleSerializer, name='apilist'),
     path("api-crud/<int:pk>/", article_detais_views, name='apicrud'),
+    path("class-api", ArticleListViewsCreateViews.as_view(), name='class-apilist'),
+    path("class-api/<int:pk>/", ArticleDetailView.as_view(), name='class-apicrud'),
     #path("api-crud/<int:pk>/", article_detais_views, name='apicrud'),
 ]
